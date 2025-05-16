84b2cc5630e046079c99512b496684d0a9f8803d
RODRIGO INACIO DA SILVA <rodrigo.isilva1@senacsp.edu.br>
2025-05-15 22:23:32 -0300

    criando lista no .js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,19 @@
 function addTask() {
-    var input = document.getElementById("input-tarefa").value
+    var input = document.getElementById("input-tarefa")
+    var inputVar = input.value
+    input.value = null
+
+    var list = document.getElementById("lista-tarefas")
+
+    // criando elemento li
+    var li = document.createElement("li")
+
+    li.innerHTML = `${inputVar} <button onclick="this.parentElement.remove()"> <img src="../../assets/lixeira.png" width="17px" height="17px"> </button> `
+
+        li.onclick = function() {
+            li.className = "tarefa-concluida"
+        }
+    // adicionando li à lista 
+
+    list.appendChild(li);
 }
